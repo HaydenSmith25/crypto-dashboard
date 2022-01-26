55f1d34317f37dfb814a0a89a3f2d9ac75876a02
Hayden Smith <hayden.smith-25@outlook.com>
2022-01-25 21:01:49 -0600

    News Fedd updated

--- src/Components/NewsFeed.js
+++ src/Components/NewsFeed.js
@@ -1,5 +1,44 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react/cjs/react.development";
+
 const NewsFeed = () => {
-  return <div className="news-feed">NewsFeed</div>;
+  const [articles, setArticles] = useState(null);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://crypto-news15.p.rapidapi.com/news",
+      headers: {
+        "x-rapidapi-host": "crypto-news15.p.rapidapi.com",
+        "x-rapidapi-key": "2cff0e9c19msh96c7d87bce15d5fp1e8c6ajsn6c0a1f532305",
+      },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data);
+        setArticles(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const firstSevenArticles = articles?.slice(0, 7);
+
+  return (
+    <div className="news-feed">
+      <h2>News Feed</h2>
+      {firstSevenArticles?.map((article, _index) => (
+        <div key={_index}>
+          <a href={article.url}>
+            <p>{article.title}</p>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default NewsFeed;
